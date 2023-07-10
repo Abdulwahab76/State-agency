078b49cd528d86b86e2d479ff5ddef685148563b
Abdulwahab76 <64549162+Abdulwahab76@users.noreply.github.com>
2023-07-10 14:32:01 -0700

    fix faqs section functionallity

--- constants/Faqs.js
+++ constants/Faqs.js
@@ -44,10 +44,10 @@ FaqsContainer.innerHTML = Faqs.map(
     (faq, ind) =>
         `<div class="border-b border-gray-200 w-full faq">
         <div class="flex w-full p-8 gap-x-10">
-        <p class="text-lg text-gray-400 font-medium">01</p>
+        <p class="text-lg text-gray-400 font-medium serial">0${ind + 1}</p>
 
           <div class="flex gap-5 flex-col basis-full">
-            <p class="text-2xl font-bold ">
+            <p class="text-2xl font-bold question" >
               ${faq.question}
             </p>
             <p class="leading-relaxed answer">
@@ -55,7 +55,7 @@ FaqsContainer.innerHTML = Faqs.map(
             </p>
           </div>
           <span
-            class="ml-auto  w-[40px] h-[30px] question  cursor-pointer bg-gray-200 rounded-full flex justify-center items-center"
+            class="ml-auto  w-[40px] h-[30px] toggle-button  cursor-pointer bg-gray-200 rounded-full flex justify-center items-center"
           >
             <p class="font-extrabold">+</p>
           </span>
@@ -65,25 +65,36 @@ FaqsContainer.innerHTML = Faqs.map(
 
 const questions = document.querySelectorAll(".question");
 const answers = document.querySelectorAll(".answer");
+const toggleButtons = document.querySelectorAll(".toggle-button");
+const serial_active = document.querySelectorAll(".serial");
 
 questions.forEach((question, index) => {
-    question.addEventListener("click", function (e) {
-        const answer = answers[index];
-        const isHidden = !answer.classList.contains("show");
-        console.log(e.target, 'hidden');
-        // Hide all answers
+    const answer = answers[index];
+    const toggleButton = toggleButtons[index];
+    const toggleSerial = serial_active[index]
+
+    toggleButton.addEventListener("click", function () {
+        const isActive = question.classList.contains("active-question");
+
+        // Hide all answers and remove active class from all questions
         answers.forEach((ans) => {
             ans.classList.remove("show");
-            question.classList.remove("active");
-            e.target.innerHTML = '+'
-
+        });
+        questions.forEach((ques) => {
+            ques.classList.remove("active-question");
+        });
+        toggleButtons.forEach((button) => {
+            button.innerHTML = "+";
+            button.classList.remove("active");
         });
 
-        // Show the clicked answer
-        if (isHidden) {
+        // Toggle the clicked question's active state and show/hide the answer
+        if (!isActive) {
             answer.classList.add("show");
-            question.classList.add('active')
-            e.target.innerHTML = '-'
+            question.classList.add("active-question");
+            toggleButton.innerHTML = "-";
+            toggleButton.classList.add("active");
+            toggleSerial.classList.add("serial-active");
         }
     });
 });
